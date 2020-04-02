636e0b91e2a16011e160a20eecd72be3b1218e29
SomeOne583 <janel.fantasma2002@gmail.com>
2020-04-02 17:30:40 -0600

    Fixing routing errors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthComponent } from './components/auth/auth.component';
 
 const routes: Routes = [
-    { path: "/login", component: AuthComponent },
-    { path: "/signup", component: AuthComponent },
-    { path: "**", redirectTo: "/login" }
+    { path: "login", component: AuthComponent },
+    { path: "signup", component: AuthComponent },
+    { path: "*", redirectTo: "login" }
 ];
 
 @NgModule({
